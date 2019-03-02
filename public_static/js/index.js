360063e1dbea57ca4dce1a99a61defca31923566
raghav-agarwal <bly.raghav@gmail.com>
2019-03-02 22:07:37 +0530

    t

--- public_static/js/index.js
+++ public_static/js/index.js
@@ -19,11 +19,14 @@ $(document).ready(() => {
     });
     
     $('#validate_trans').click(() => {
-        var selectedAccount ='0xeac5a6b2b9ffe4459165b8da862c56fe0bfe93c2';
+        $.post('/getAccounts', {}, function (response) {
+            console.log("done hai :)");
+        })
+        /*var selectedAccount ='0xeac5a6b2b9ffe4459165b8da862c56fe0bfe93c2';
         $.post('/setValidator', {account : selectedAccount}, function (response) {
         console.log("done hai :)"+response);
 
-    })  
+    })  */
         
     });
 
